778ad1bf21f60ff2a1342f1507ecec10c3f197b9
Brandon Foster <Brandon.Foster4525@outlook.com>
2024-11-05 04:11:09 -0500

    made the Element class an abstract class.
    made the Element class an abstract class.
    added getElementType() virtual function.
    added toString() virtual function.
    added as() template function for safe typecasting.

--- api/c++/include/softloq-json/element.hpp
+++ api/c++/include/softloq-json/element.hpp
@@ -1,15 +1,21 @@
 #ifndef SOFTLOQ_JSON_ELEMENT_HPP
 #define SOFTLOQ_JSON_ELEMENT_HPP
 
+#include <string>
 #include <softloq-json/definitions.hpp>
 
 namespace softloq::json {
-    
+
 // JSON Elements are based on https://www.json.org
 
 enum class ElementType { Object, Array, String, Number, Bool, Null };
 
-class SOFTLOQ_JSON_API Element {};
+class SOFTLOQ_JSON_API Element {
+public:
+    virtual const ElementType getElementType() const = 0;
+    virtual const std::string toString() const = 0;
+    template <class ELEMENT_TYPE> ELEMENT_TYPE* const as() { return dynamic_cast<ELEMENT_TYPE*>(this); }
+};
 
 class SOFTLOQ_JSON_API Object: public Element {};
 class SOFTLOQ_JSON_API Array: public Element {};
