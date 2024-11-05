5a1ab83ac71e9c23a37d80b81aa3a6780da876ef
Brandon Foster <Brandon.Foster4525@outlook.com>
2024-11-05 04:27:41 -0500

    implemented the getElementType() function for json elements.

--- api/c++/include/softloq-json/element.hpp
+++ api/c++/include/softloq-json/element.hpp
@@ -17,12 +17,35 @@ public:
     template <class ELEMENT_TYPE> ELEMENT_TYPE* const as() { return dynamic_cast<ELEMENT_TYPE*>(this); }
 };
 
-class SOFTLOQ_JSON_API Object: public Element {};
-class SOFTLOQ_JSON_API Array: public Element {};
-class SOFTLOQ_JSON_API String: public Element {};
-class SOFTLOQ_JSON_API Number: public Element {};
-class SOFTLOQ_JSON_API Bool: public Element {};
-class SOFTLOQ_JSON_API Null: public Element{};
+class SOFTLOQ_JSON_API Object: public Element {
+public:
+    inline const ElementType getElementType() const override { return ElementType::Object; }
+};
+
+class SOFTLOQ_JSON_API Array: public Element {
+public:
+    inline const ElementType getElementType() const override { return ElementType::Array; }
+};
+
+class SOFTLOQ_JSON_API String: public Element {
+public:
+    inline const ElementType getElementType() const override { return ElementType::String; }
+};
+
+class SOFTLOQ_JSON_API Number: public Element {
+public:
+    inline const ElementType getElementType() const override { return ElementType::Number; }
+};
+
+class SOFTLOQ_JSON_API Bool: public Element {
+public:
+    inline const ElementType getElementType() const override { return ElementType::Bool; }
+};
+
+class SOFTLOQ_JSON_API Null: public Element{
+public:
+    inline const ElementType getElementType() const override { return ElementType::Null; }
+};
 
 } // namespace softloq::json
 
